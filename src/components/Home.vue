8863b1c7d08e63ab13e1eba837d15b789e566949
Zhang <18310701744@163.com>
2020-04-23 10:29:41 +0800

    完成了登陆功能

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    home组件
+    <el-button type="info" round @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
